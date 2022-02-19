7d1501426bc9a760405a2c1f813f7b38ea4f83a4
JoaoPedroSkalski <97605230+JoaoPedroSkalski@users.noreply.github.com>
2022-02-19 09:24:36 -0300

    Add files via upload

---
+++ projeto 16 do 2 2022/scripts.js
@@ -0,0 +1,18 @@
+function boss1(a){
+    document.getElementById("imgMostra").src=a;
+    document.getElementById("string1").innerHTML=("Nome: Vigilantes do abismo <br> Localização: No forte de Farron, após uma grande porta de pedra <br>  Os Vigilantes do Abismo são Mortos-Vivos de Farron, que descobriram que sozinhos não seriam capazes deter o Abismo, então juraram pelos seus Sangue de Lobo que usariam todas as suas forças para deter o Abismo! Eles são mortos-vivos altos, usam uma armadura extremamente leve e um conjunto de duas espadas como arma, uma Espada Curta para aparar e uma Espada Grande Esplêndida para atacar em seguida. <br> Drops: Alma do Sangue do lobo & cinzas de um lorde <br> <img src='alma1.png' width='70px' heigth='70px'> <img src='cinzas1.png' width='70px' heigth='70px'> ")
+}
+function boss2(b){
+    document.getElementById("imgMostra").src=b;
+    document.getElementById("string1").innerHTML=("")
+}
+function boss3(c){
+    document.getElementById("imgMostra").src=c;
+    document.getElementById("string1").innerHTML=("")
+}
+function boss4(d){
+    document.getElementById("imgMostra").src=d;
+    document.getElementById("string1").innerHTML=("")
+}
+
+
